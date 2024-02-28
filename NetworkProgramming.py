a2189ece837e6ce23915d4ea5e047b3011249cf8
LuisCabral98 <101224196+LuisCabral98@users.noreply.github.com>
2024-02-28 16:22:44 -0500

    Delete NetworkProgramming.py

--- NetworkProgramming.py
+++
@@ -1,9 +0,0 @@
-# Socket Programming
-# method of communication between two nodes or sockets
-
-import socket
-import sys
-
-# first parameter refers to address IPv4 and second parameter is connection oriented TCP 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# to get ip for website, type ping and then your website 
